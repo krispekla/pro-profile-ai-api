d671bf913ad71679c830498969fc06b2dc9cc998
krispekla <peklarick@gmail.com>
2023-11-09 08:13:13 +0100

    Add config structure and flag for address argument

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
+type Config struct {
+	Addr string
+}
+
 func main() {
+	cfg := &Config{}
+	flag.StringVar(&cfg.Addr, "addr", ":3002", "Port to run this service on")
+	flag.Parse()
+
 	r := chi.NewRouter()
 	r.Get("/ping", ping)
 	srv := &http.Server{
-		Addr:    ":3002",
+		Addr:    cfg.Addr,
 		Handler: r,
 	}
 	log.Println("Starting server on port ", srv.Addr)
