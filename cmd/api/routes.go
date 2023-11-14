1eabc6d42157a3aaa02049253dda5a0312ab9679
krispekla <peklarick@gmail.com>
2023-11-14 22:45:35 +0100

    Add logger middleware to log all routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/krispekla/pro-profile-ai-api/config"
 )
 
 func routes(app *config.Application) *chi.Mux {
 	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 	r.Use(middleware.Heartbeat("/ping"))
 	r.Post("/login", login(app))
 	r.Post("/register", register(app))
