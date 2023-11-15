76461977929e8ddb1d970ce0115bc7fe08dad224
krispekla <peklarick@gmail.com>
2023-11-15 13:04:50 +0100

    Comment unused handlers

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,48 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/krispekla/pro-profile-ai-api/config"
 )
 
-func login(app *config.Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Get the username and password from the request body
-		email := r.PostFormValue("email")
-		password := r.PostFormValue("password")
-		// Check if the email and password are valid
+// func login(app *config.Application) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		// Get the username and password from the request body
+// 		email := r.PostFormValue("email")
+// 		password := r.PostFormValue("password")
+// 		// Check if the email and password are valid
 
-		if email == "" || password == "" {
-			app.ClientError(w, http.StatusBadRequest)
-			return
-		}
+// 		if email == "" || password == "" {
+// 			app.ClientError(w, http.StatusBadRequest)
+// 			return
+// 		}
 
-		msg := fmt.Sprintf("User %s is logged in", email)
+// 		msg := fmt.Sprintf("User %s is logged in", email)
 
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(msg))
-	}
-}
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Write([]byte(msg))
+// 	}
+// }
 
-func register(app *config.Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		firstName := r.PostFormValue("first_name")
-		lastName := r.PostFormValue("last_name")
-		email := r.PostFormValue("email")
-		password := r.PostFormValue("password")
+// func register(app *config.Application) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		firstName := r.PostFormValue("first_name")
+// 		lastName := r.PostFormValue("last_name")
+// 		email := r.PostFormValue("email")
+// 		password := r.PostFormValue("password")
 
-		if firstName == "" || lastName == "" || email == "" || password == "" {
-			app.ClientError(w, http.StatusBadRequest)
-			return
-		}
+// 		if firstName == "" || lastName == "" || email == "" || password == "" {
+// 			app.ClientError(w, http.StatusBadRequest)
+// 			return
+// 		}
 
-		w.WriteHeader(http.StatusCreated)
-		uCreatedMsg := fmt.Sprintf("User created: %s %s", firstName, lastName)
-		w.Write([]byte(uCreatedMsg))
-	}
-}
+// 		w.WriteHeader(http.StatusCreated)
+// 		uCreatedMsg := fmt.Sprintf("User created: %s %s", firstName, lastName)
+// 		w.Write([]byte(uCreatedMsg))
+// 	}
+// }
 
 func userDetails(app *config.Application) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
