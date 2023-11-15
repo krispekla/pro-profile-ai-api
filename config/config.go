255ecdd2d7df1a6534081cae55ee164e6f6e7e17
krispekla <peklarick@gmail.com>
2023-11-15 12:35:47 +0100

    Define additional configs

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"log"
 	"os"
 
@@ -8,12 +9,30 @@ import (
 )
 
 type Application struct {
-	Addr     string
-	ErrorLog *log.Logger
-	InfoLog  *log.Logger
+	Addr       string
+	DbHost     string
+	DbPort     string
+	DbName     string
+	DbUser     string
+	DbPassword string
+	JwtSecret  string
+	ErrorLog   *log.Logger
+	InfoLog    *log.Logger
 }
 
 func (app *Application) CreateLoggers() {
 	app.InfoLog = log.New(os.Stdout, color.GreenString("INFO\t"), log.Ldate|log.Ltime)
 	app.ErrorLog = log.New(os.Stderr, color.RedString("ERROR\t"), log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func (app *Application) LoadConfig() {
+	// TODO: Replace with viper
+	flag.StringVar(&app.Addr, "addr", ":3002", "Port to run this service on")
+	flag.StringVar(&app.DbHost, "db-host", "localhost", "Database host")
+	flag.StringVar(&app.DbPort, "db-port", "5432", "Database port")
+	flag.StringVar(&app.DbName, "db-name", "", "Database name")
+	flag.StringVar(&app.DbUser, "db-user", "", "Database user")
+	flag.StringVar(&app.DbPassword, "db-password", "", "Database password")
+	flag.StringVar(&app.JwtSecret, "jwt-secret", "", "JWT secret for checking token validity")
+	flag.Parse()
 }
