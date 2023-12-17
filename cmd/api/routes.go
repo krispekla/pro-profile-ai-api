77522c6d5e853b8da30d1465914beb6a41ce0b24
krispekla <peklarick@gmail.com>
2023-12-17 12:46:41 +0100

    WIP

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,7 +12,7 @@ func routes(app *config.Application) *chi.Mux {
 	r.Use(middleware.Recoverer)
 	r.Use(CorsMiddleware(app))
 	r.Use(middleware.Heartbeat("/ping"))
-	// r.Use(AuthMiddleware(app))
+	r.Use(AuthMiddleware(app))
 	r.Route("/api", func(r chi.Router) {
 		r.Get("/user", userDetails(app))
 		r.Get("/characters", getCharacters(app))
