90f10a4f518b7ccda8bf1140706b87bb6d3c0409
krispekla <peklarick@gmail.com>
2024-01-09 19:54:37 +0100

    Add package generated dto

--- types/package.go
+++ types/package.go
@@ -1,10 +1,20 @@
 package types
 
 import (
+	"time"
+
 	"github.com/krispekla/pro-profile-ai-api/.gen/postgres/public/model"
 )
 
 type PackageListingDTO struct {
 	*model.Package
 	Pricing *model.PackagePrice `alias:"package_price"`
+}
+
+type PackageGeneratedDTO struct {
+	Id          int32                        `alias:"generated_package.id" json:"id"`
+	Status      model.GeneratedPackageStatus `alias:"generated_package.status" json:"status"`
+	CoverImgURL *string                      `alias:"generated_package.cover_img_url" json:"cover_img_url"`
+	Updated     time.Time                    `alias:"generated_package.updated" json:"updated"`
+	PackageId   *int64                       `alias:"package_order_item.package_id" json:"package_id"`
 }
