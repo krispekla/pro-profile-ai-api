38dec4261c6b3f35889c332cf0ae04fda3210d2d
krispekla <peklarick@gmail.com>
2024-01-15 19:55:32 +0100

    Get package prices by stripe product ids

--- internal/repository/package.go
+++ internal/repository/package.go
@@ -6,6 +6,7 @@ import (
 
 	. "github.com/go-jet/jet/v2/postgres"
 	"github.com/google/uuid"
+	"github.com/krispekla/pro-profile-ai-api/.gen/postgres/public/model"
 	. "github.com/krispekla/pro-profile-ai-api/.gen/postgres/public/table"
 	"github.com/krispekla/pro-profile-ai-api/types"
 	_ "github.com/lib/pq"
@@ -14,6 +15,7 @@ import (
 type PackageRepository interface {
 	GetListing() (*[]types.PackageListingDTO, error)
 	GetGeneratedPackages(usrId uuid.UUID) (*[]types.PackageGeneratedDTO, error)
+	GetPackagePrice(productId *[]string) (*[]model.PackagePrice, error)
 }
 
 type PackageRepositoryImpl struct {
@@ -53,6 +55,24 @@ func (r *PackageRepositoryImpl) GetGeneratedPackages(usrId uuid.UUID) (*[]types.
 	)
 
 	var result []types.PackageGeneratedDTO
+
+	err := stmt.Query(r.Db, &result)
+	if err != nil {
+		return nil, errors.New("error retrieving packages")
+	}
+	return &result, nil
+}
+
+func (r *PackageRepositoryImpl) GetPackagePrice(productIds *[]string) (*[]model.PackagePrice, error) {
+	var pIds []Expression
+	for _, pId := range *productIds {
+		pIds = append(pIds, String(pId))
+	}
+	stmt := SELECT(
+		PackagePrice.AllColumns,
+	).FROM(PackagePrice).WHERE(PackagePrice.StripeProductID.IN(pIds...))
+
+	var result []model.PackagePrice
 
 	err := stmt.Query(r.Db, &result)
 	if err != nil {
